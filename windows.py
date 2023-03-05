a676ccd653b7d3d27f6de241fa28d60cadf1e316
Discomoth <raineyratcliff@gmail.com>
2023-03-04 17:07:23 -0800

    Added rts/cts radio button, renamed combobox.

--- windows.py
+++ windows.py
@@ -4,6 +4,7 @@ from PyQt6 import QtWidgets, uic
 from PyQt6.QtGui import QAction
 from PyQt6.QtTest import QTest
 import os, time
+
 from plotterHandler import plotterSerial
 
 
@@ -41,33 +42,43 @@ class mainWindow(QtWidgets.QMainWindow):
 
 
 class plotterConfigWindow(QtWidgets.QDialog):
-    plotterSerial()
+    
+    serialObject = None
 
     def __init__(self):
         super(plotterConfigWindow, self).__init__()
         uic.loadUi('windows/plotterConfig.ui', self)
         self.show()
 
+        self.serialObject = plotterSerial()
+
         # Configuration
         self.combo_serialPort = self.findChild(
             QtWidgets.QComboBox, 'combo_serialPort')
-        self.combo_serialPort.addItems(plotterSerial.portList)
+        self.combo_serialPort.addItems([str(port[0] + ', ' + port[1]) for port in plotterSerial.portList])
 
         self.button_serialRefresh = self.findChild(
             QtWidgets.QPushButton, 'button_serialPortRefresh')
         self.button_serialRefresh.clicked.connect(self.serialRefresh)
 
         self.combo_baudRate = self.findChild(
-            QtWidgets.QComboBox, 'combo_baudRate')
+            QtWidgets.QComboBox, 'combo_baud')
+        self.combo_baudRate.addItems(plotterSerial.baudRates)
 
         self.doubleSpin_timeout = self.findChild(
             QtWidgets.QDoubleSpinBox, 'doubleSpin_timeout')
 
         self.radio_xonxoff = self.findChild(
             QtWidgets.QRadioButton, 'radio_xonxoff')
+        self.radio_xonxoff.setChecked(plotterSerial.xonxoff)
 
         self.radio_dsrdtr = self.findChild(
             QtWidgets.QRadioButton, 'radio_dsrdtr')
+        self.radio_dsrdtr.setChecked(plotterSerial.dsrdtr)
+        
+        self.radio_rtscts = self.findChild(
+            QtWidgets.QRadioButton, 'radio_rtscts')
+        self.radio_rtscts.setChecked(plotterSerial.rtscts)
 
         self.radio_none = self.findChild(
             QtWidgets.QRadioButton, 'radio_none')
@@ -97,13 +108,25 @@ class plotterConfigWindow(QtWidgets.QDialog):
         self.button_serialRefresh.setDisabled(False)
         self.combo_serialPort.setDisabled(False)
         self.combo_serialPort.clear()
-        self.combo_serialPort.addItems(plotterSerial.portList)
-
-        
+        self.combo_serialPort.addItems([str(port[0] + ', ' + port[1]) for port in plotterSerial.portList])
 
     def testConnect(self):
         print('testConnect!')
 
+        plotterSerial.serialPort = plotterSerial.portList[self.combo_serialPort.currentIndex()][0]
+        plotterSerial.baudRate = int(plotterSerial.baudRates[self.combo_baudRate.currentIndex()])
+        plotterSerial.timeout = self.doubleSpin_timeout.value()
+        plotterSerial.xonxoff = self.radio_xonxoff.isChecked()
+        plotterSerial.dsrdtr = self.radio_dsrdtr.isChecked()
+        plotterSerial.rtscts = self.radio_rtscts.isChecked()
+
         self.button_connect.setDisabled(True)
-        QTest.qWait(1000)
+
+        try:
+            self.serialObject.connect()
+            self.text_textConnect.setText(plotterSerial.model)
+        except Exception as e:
+            print(e)
+            self.text_textConnect.setText('Failed To Connect!')
+
         self.button_connect.setDisabled(False)
